651b9a491ec2383284b36cfcf421a9c45569e5a9
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-04-19 18:59:22 +0200

    MORE ARGUMENT COMPLETION!!!

--- PSTranslate/internal/tepp/assignment.ps1
+++ PSTranslate/internal/tepp/assignment.ps1
@@ -1,2 +1,3 @@
 ﻿Register-PSFTeppArgumentCompleter -Command Get-TranslationApiKey -Parameter Provider -Name PSTranslate.Provider
 Register-PSFTeppArgumentCompleter -Command Add-TranslationApiKey -Parameter Provider -Name PSTranslate.Provider
+Register-PSFTeppArgumentCompleter -Command Get-Translation -Parameter Provider -Name PSTranslate.Provider
